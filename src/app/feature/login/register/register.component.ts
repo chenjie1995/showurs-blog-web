3922658277771ad2cd7778f31efffcf3d41cdc4d
chenjie <chenjie1995@live.com>
2019-08-11 22:36:27 +0800

    登录及注册页面调整

--- src/app/feature/login/register/register.component.ts
+++ src/app/feature/login/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from 'src/app/core/service/config.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormService } from 'src/app/core/service/form.service';
 
 const userUrl: string = 'user/users';
 
@@ -11,28 +12,41 @@ const userUrl: string = 'user/users';
 })
 export class RegisterComponent implements OnInit {
 
-    passwordVisible: boolean;
     isLoadingRegisterButton: boolean;
     captchaUrl: string;
     userRegister: FormGroup;
 
     constructor(private configService: ConfigService,
-        private fb: FormBuilder) {
-        this.passwordVisible = true;
+        private fb: FormBuilder,
+        private formService: FormService) {
         this.isLoadingRegisterButton = false;
         this.captchaUrl = `${this.configService.getApiUrl()}/${userUrl}/captcha-image`;
         this.userRegister = this.fb.group({
-            username: ['', [Validators.required]],
+            username: ['', [Validators.required, Validators.maxLength(18)]],
             email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required]],
-            captcha: ['', [Validators.required]]
+            password: ['', [Validators.required, Validators.maxLength(18)]],
+            confirmPassword: ['', [Validators.required, Validators.maxLength(18), this.confirmValidator]],
+            captcha: ['', [Validators.required, Validators.maxLength(6)]]
         });
     }
 
     ngOnInit() {
     }
 
-    register() {
+    confirmValidator = (control: FormControl): { [name: string]: boolean } => {
+        if (!control.value) {
+            return { required: true };
+        } else if (control.value !== this.userRegister.controls.password.value) {
+            return { confirm: true, error: true };
+        }
+        return {};
+    };
 
+    register() {
+        if (this.userRegister.valid) {
+            console.log('1234');
+        } else {
+            this.formService.showValidInfo(this.userRegister);
+        }
     }
 }
