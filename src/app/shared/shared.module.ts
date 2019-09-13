d0230af1a433d6e541d65355fb766609f5ebffdb
chenjie <chenjie1995@live.com>
2019-09-13 12:42:59 +0800

    同步代码

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        NavComponent
+    ],
     imports: [
-        CommonModule
+        CommonModule,
+        NgZorroAntdModule
+    ],
+    exports: [
+        NavComponent
     ]
 })
 export class SharedModule { }
